79fccf15673e1194f380e5c86579a0d3ee6dfc6f
Nguyen Tu <tuunguyen2795@gmail.com>
2017-01-07 09:26:33 +0700

    Updated send image

--- app.js
+++ app.js
@@ -79,6 +79,11 @@ app.use('/posts', posts);
 app.use('/categories', categories);
 app.use('/', index);
 
+app.get('/public/uploads/:image', function (req, res) {
+    console.log(req.params.image);
+    res.sendFile(__dirname + '/public/uploads/'+req.params.image);
+});
+
 app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
